6d7b8818f64dab2ce53b34b23f98ad60e2bd2a6b
Kateryna Karpova <katyakarpova@gmail.com>
2026-02-04 23:49:23 +0100

    task 2

--- final_project.sql
+++ final_project.sql
@@ -1,28 +1,28 @@
--- 1. Створення схеми та вибір її за замовчуванням
+-- П.1 1. Створення схеми та вибір її за замовчуванням
 
 CREATE SCHEMA IF NOT EXISTS pandemic;
 
 USE pandemic;
 
--- 2. Перегляд імпортованих даних
+-- П.1 2. Перегляд імпортованих даних
 SELECT * FROM infectious_cases;
 
--- 3. Створення таблиці для унікальних Entity та Code
+-- П.1 3. Створення таблиці для унікальних Entity та Code
 CREATE TABLE entities (
 id INT AUTO_INCREMENT PRIMARY KEY,
 entity TEXT,
 code TEXT
 );
 
--- 4. Заповнення таблиці entities унікальними значеннями Entity та Code
+-- П.1 4. Заповнення таблиці entities унікальними значеннями Entity та Code
 INSERT INTO entities (entity, code)
 SELECT DISTINCT Entity, Code
 FROM infectious_cases;
 
--- 5. Перевірка таблиці entities
+-- П.1 5. Перевірка таблиці entities
 SELECT * FROM entities;
 
--- 6. Створення нової таблиці без дублювання Entity та Code, замінюючи їх на унікальний ідентифікатор entity_id
+-- П.1 6. Створення нової таблиці без дублювання Entity та Code, замінюючи їх на унікальний ідентифікатор entity_id
 CREATE TABLE infectious_cases_no_entity_code AS
 SELECT
   e.id AS entity_id,
@@ -40,5 +40,93 @@ FROM infectious_cases ic
 JOIN entities e
   ON ic.Entity = e.entity AND ic.Code = e.code;
 
--- 7. Перевірка нової таблиці
+-- П.1 7. Перевірка нової таблиці
 SELECT * FROM infectious_cases_no_entity_code
+
+-- П.2 1. Підрахунок кількості записів у таблиці infectious_cases_no_entity_code
+SELECT COUNT(*) FROM infectious_cases_no_entity_code
+
+-- П.2 2. Створення таблиці diseases для унікальних назв хвороб
+CREATE TABLE diseases (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL UNIQUE
+);
+
+-- П.2 3. Заповнення таблиці diseases
+
+INSERT INTO diseases (name) VALUES
+	('yaws'),
+	('polio'),
+	('guinea_worm'),
+	('rabies'),
+	('malaria'),
+	('hiv'),
+	('tuberculosis'),
+	('smallpox'),
+	('cholera');
+
+-- П.2 4. Перевірка таблиці diseases
+    
+SELECT * FROM diseases
+
+-- П.2 5. Створення таблиці infectious_cases_normalized з нормалізованою структурою
+CREATE TABLE infectious_cases_normalized (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  entity_id INT NOT NULL,
+  year INT NOT NULL,
+  disease_id INT NOT NULL,
+  cases TEXT,
+  FOREIGN KEY (entity_id) REFERENCES entities(id),
+  FOREIGN KEY (disease_id) REFERENCES diseases(id)
+);
+
+-- П.2 6. Заповнення таблиці infectious_cases_normalized
+INSERT INTO infectious_cases_normalized (entity_id, year, disease_id, cases)
+
+SELECT entity_id, Year, d.id, Number_yaws
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'yaws'
+
+UNION ALL
+SELECT entity_id, Year, d.id, polio_cases
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'polio'
+
+UNION ALL
+SELECT entity_id, Year, d.id, cases_guinea_worm
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'guinea_worm'
+
+UNION ALL
+SELECT entity_id, Year, d.id, Number_rabies
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'rabies'
+
+UNION ALL
+SELECT entity_id, Year, d.id, Number_malaria
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'malaria'
+
+UNION ALL
+SELECT entity_id, Year, d.id, Number_hiv
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'hiv'
+
+UNION ALL
+SELECT entity_id, Year, d.id, Number_tuberculosis
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'tuberculosis'
+
+UNION ALL
+SELECT entity_id, Year, d.id, Number_smallpox
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'smallpox'
+
+UNION ALL
+SELECT entity_id, Year, d.id, Number_cholera_cases
+FROM infectious_cases_no_entity_code ic
+JOIN diseases d ON d.name = 'cholera';
+
+-- П.2 7. Перевірка таблиці infectious_cases_normalized
+SELECT * FROM infectious_cases_normalized;
+
